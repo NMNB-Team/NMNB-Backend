6a5760f4dc81b0fb864d0a132434339b648aee40
88dldl <chltnrgml0999@gmail.com>
2025-04-24 15:19:48 +0900

    feat: 공통 에러, 성공 상태 코드 Enum 클래스 정의

---
+++ src/main/kotlin/cv/nmnb/global/response/status/SuccessStatus.kt
@@ -0,0 +1,20 @@
+package cv.nmnb.global.response.status
+
+import cv.nmnb.global.response.base.BaseCode
+import cv.nmnb.global.response.dto.ReasonDTO
+import org.springframework.http.HttpStatus
+
+enum class SuccessStatus(
+    val httpStatus: HttpStatus,
+    val code: String,
+    val message: String,
+) : BaseCode {
+    OK(HttpStatus.OK, "COMMON200", "성공입니다."),
+    CREATED(HttpStatus.CREATED, "COMMON201", "요청 성공 및 리소스 생성됨"),
+    NO_CONTENT(HttpStatus.NO_CONTENT, "COMMON202", "요청 성공 및 반환할 콘텐츠가 없음"),
+    ;
+
+    override fun getReasonHttpStatus(): ReasonDTO {
+        return ReasonDTO(httpStatus, code, message)
+    }
+}
