f7ad6ad6473a620c17de47346f3d585ecbe1e0e1
88dldl <chltnrgml0999@gmail.com>
2025-04-25 12:39:50 +0900

    chore: UserController와 UserService 단위 테스트 지원

--- src/test/kotlin/cv/nmnb/ControllerTestSupport.kt
+++ src/test/kotlin/cv/nmnb/ControllerTestSupport.kt
@@ -1,11 +1,21 @@
 package cv.nmnb
 
+import cv.nmnb.domain.user.controller.UserController
+import cv.nmnb.domain.user.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 
-@WebMvcTest
+@WebMvcTest(
+    controllers = [
+        UserController::class,
+    ],
+)
 abstract class ControllerTestSupport {
     @Autowired
     protected lateinit var mockMvc: MockMvc
+
+    @MockBean
+    protected lateinit var userService: UserService
 }
