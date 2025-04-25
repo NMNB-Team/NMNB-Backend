804e0afc34db8038966957d74e5e4183e2927db0
88dldl <chltnrgml0999@gmail.com>
2025-04-25 12:38:30 +0900

    feat: 파생 프로퍼티 추가 및 테스트용 fixture 메서드 정의

--- src/main/kotlin/cv/nmnb/domain/user/domain/User.kt
+++ src/main/kotlin/cv/nmnb/domain/user/domain/User.kt
@@ -27,11 +27,37 @@ class User(
     @Id
     @field:UserId
     @Column(name = "user_id")
-    val id: String? = null
+    var id: String? = null
 
     @Enumerated(EnumType.STRING)
     var petOwnershipStatus: PetOwnershipStatus = PetOwnershipStatus.NO_PET
 
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
     var posts: MutableList<Post> = mutableListOf()
+
+    val nickName: String
+        get() = companionAnimal?.let { companionAnimal -> "$companionAnimal-$id" } ?: id.toString()
+    val hasAnimal: Boolean
+        get() = petOwnershipStatus == PetOwnershipStatus.HAS_PET
+
+    companion object {
+        fun fixture(
+            id: String? = null,
+            email: String = "ex@example.com",
+            profileImage: String = "default.png",
+            companionAnimal: String? = null,
+            petOwnershipStatus: PetOwnershipStatus = PetOwnershipStatus.NO_PET,
+            posts: MutableList<Post> = mutableListOf(),
+        ): User {
+            val user = User(
+                email = email,
+                profileImage = profileImage,
+                companionAnimal = companionAnimal,
+            )
+            user.id = id
+            user.petOwnershipStatus = petOwnershipStatus
+            user.posts = posts
+            return user
+        }
+    }
 }
