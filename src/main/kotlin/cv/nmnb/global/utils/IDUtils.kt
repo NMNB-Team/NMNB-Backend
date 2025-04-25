41986ade2c97670acb10c03cc8feb880027f0e38
88dldl <chltnrgml0999@gmail.com>
2025-04-25 11:07:10 +0900

    refactor: UUID에서 NanoID로 ID 생성 방식 변경하여 중복 가능성 줄이기

--- src/main/kotlin/cv/nmnb/global/utils/IDUtils.kt
+++ src/main/kotlin/cv/nmnb/global/utils/IDUtils.kt
@@ -1,17 +1,15 @@
 package cv.nmnb.global.utils
 
-import java.util.UUID
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 
 object IDUtils {
     private const val ID_LENGTH: Int = 16
-
+    private const val CUSTOM_ALPHABET_STRING = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#\$%^&*"
     fun customIdGenerator(): String {
-        val symbols = listOf('!', '@', '#', '$', '%', '&', '*')
-
-        return UUID.randomUUID()
-            .toString()
-            .map { if (it == '-') symbols.random() else it }
-            .joinToString("")
-            .take(ID_LENGTH)
+        return NanoIdUtils.randomNanoId(
+            NanoIdUtils.DEFAULT_NUMBER_GENERATOR,
+            CUSTOM_ALPHABET_STRING.toCharArray(),
+            ID_LENGTH,
+        )
     }
 }
