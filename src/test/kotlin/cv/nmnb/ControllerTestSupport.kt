1288444f3c0d11d8074c08509add6b24b26b574a
88dldl <chltnrgml0999@gmail.com>
2025-04-25 11:37:16 +0900

    test: 공통 테스트 환경 설정 코드 작성

---
+++ src/test/kotlin/cv/nmnb/ControllerTestSupport.kt
@@ -0,0 +1,11 @@
+package cv.nmnb
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+
+@WebMvcTest
+abstract class ControllerTestSupport {
+    @Autowired
+    protected lateinit var mockMvc: MockMvc
+}
