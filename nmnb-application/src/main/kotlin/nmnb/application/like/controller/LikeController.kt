ddd1d0976cf554a2f4fb647caefd2fdb88c827b8
88dldl <chltnrgml0999@gmail.com>
2025-04-29 19:38:27 +0900

    feat: 좋아요 등록 및 취소 API 구현

---
+++ nmnb-application/src/main/kotlin/nmnb/application/like/controller/LikeController.kt
@@ -0,0 +1,31 @@
+package nmnb.application.like.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import nmnb.application.like.controller.dto.request.PostLikeRequest
+import nmnb.application.like.service.LikeService
+import nmnb.common.response.base.BaseResponse
+import nmnb.common.response.status.SuccessStatus
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Tag(name = "Like 🫰", description = "좋아요 관련 API")
+@RequestMapping("/v1/api")
+class LikeController(
+    private val likeService: LikeService,
+) {
+    @Operation(summary = "좋아요 등록/취소 API", description = "좋아요가 등록되어있으면 취소, 좋아요가 없으면 등록합니다._숙희")
+    @ApiResponses(ApiResponse(responseCode = "COMMON200", description = "성공입니다."))
+    @PatchMapping("/like")
+    fun likeOrUnlike(@RequestBody request: PostLikeRequest): BaseResponse<Any> {
+        return BaseResponse.onSuccess(
+            SuccessStatus.OK,
+            likeService.likeOrUnlike(request.user, request.toServiceRequest()),
+        )
+    }
+}
