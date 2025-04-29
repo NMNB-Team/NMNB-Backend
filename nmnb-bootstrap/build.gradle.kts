3c7124004191d230b5830f8a466c0c3da53b437c
88dldl <chltnrgml0999@gmail.com>
2025-04-29 19:38:09 +0900

    chore: JPA 및 domain 모듈 의존성 추가

--- nmnb-bootstrap/build.gradle.kts
+++ nmnb-bootstrap/build.gradle.kts
@@ -1,9 +1,11 @@
 dependencies {
     implementation(project(":nmnb-application"))
+    implementation(project(":nmnb-domain"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
 
 tasks.bootJar {
