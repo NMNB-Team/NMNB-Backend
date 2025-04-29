31b4deb1eed23b176996783a8a3f0e87e78cf58b
88dldl <chltnrgml0999@gmail.com>
2025-04-29 19:39:42 +0900

    chore: LikeController와 LikeService 단위 테스트 지원

--- nmnb-application/src/test/kotlin/nmnb/application/ControllerTestSupport.kt
+++ nmnb-application/src/test/kotlin/nmnb/application/ControllerTestSupport.kt
@@ -1,5 +1,7 @@
 package nmnb.application
 
+import nmnb.application.like.controller.LikeController
+import nmnb.application.like.service.LikeService
 import nmnb.application.user.controller.UserController
 import nmnb.application.user.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc
 @WebMvcTest(
     controllers = [
         UserController::class,
+        LikeController::class,
     ],
 )
 @ContextConfiguration(classes = [TestApplication::class])
@@ -20,4 +23,7 @@ abstract class ControllerTestSupport {
 
     @MockBean
     protected lateinit var userService: UserService
+
+    @MockBean
+    protected lateinit var likeService: LikeService
 }
