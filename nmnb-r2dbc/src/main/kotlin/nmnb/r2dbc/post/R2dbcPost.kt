45a18306ef28a5b2d2f13fed34ba5eb88d7733cb
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:09:08 +0900

    feat: R2DBC를 위한 Post 엔티티 클래스 생성

---
+++ nmnb-r2dbc/src/main/kotlin/nmnb/r2dbc/post/R2dbcPost.kt
@@ -0,0 +1,37 @@
+package nmnb.r2dbc.post
+
+import nmnb.r2dbc.R2dbcBaseEntity
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("posts")
+data class R2dbcPost(
+    @Column("url")
+    val url: String,
+
+    @Column("thumbnail_url")
+    val thumbnailUrl: String,
+
+    @Column("description")
+    var description: String? = null,
+
+    @Column("user_id")
+    val userId: String? = null,
+
+    @Id
+    @Column("post_id")
+    val id: Long? = null,
+) : R2dbcBaseEntity() {
+
+    companion object {
+        fun fixture(
+            url: String = "url",
+            thumbnailUrl: String = "thumbnail",
+            description: String? = null,
+            userId: String? = null,
+        ): R2dbcPost {
+            return R2dbcPost(url, thumbnailUrl, description, userId)
+        }
+    }
+}
