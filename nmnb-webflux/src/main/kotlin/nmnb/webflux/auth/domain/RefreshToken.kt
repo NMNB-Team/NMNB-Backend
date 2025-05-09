8009d07591dc17d8b3d96eb21fd11d0574c0ef9b
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:39:13 +0900

    feat: Redis 기반의 RefreshToken 엔티티 추가

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/auth/domain/RefreshToken.kt
@@ -0,0 +1,14 @@
+package nmnb.webflux.auth.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("refresh_token")
+@RedisHash("refresh_token")
+data class RefreshToken(
+    @Id
+    val userId: String,
+
+    var refreshToken: String,
+)
