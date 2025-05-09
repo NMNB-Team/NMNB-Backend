d42625790f224c8af43644caf0e1a8be2cca3d50
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:40:45 +0900

    feat: Spring Security 사용자 인증을 위한 CustomUserDetails 구현 추가

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/auth/domain/CustomUserDetails.kt
@@ -0,0 +1,20 @@
+package nmnb.webflux.auth.domain
+
+import nmnb.r2dbc.user.R2dbcUser
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class CustomUserDetails(
+    private val user: R2dbcUser,
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf(SimpleGrantedAuthority("ROLE_USER"))
+    }
+
+    override fun getPassword(): String {
+        return ""
+    }
+
+    override fun getUsername(): String = user.email
+}
