f644fe03c40718910bbaba7e7de76b698068a43d
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:14:02 +0900

    feat: S3AsyncClient 설정을 위한 S3Config 클래스 추가

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/config/S3Config.kt
@@ -0,0 +1,23 @@
+package nmnb.webflux.config
+
+import nmnb.webflux.common.properties.S3Properties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3AsyncClient
+
+@Configuration
+class S3Config(
+    private val s3Properties: S3Properties,
+) {
+    @Bean
+    fun s3AsyncClient(): S3AsyncClient {
+        val credentials = AwsBasicCredentials.create(s3Properties.credentials.accessKey, s3Properties.credentials.secretKey)
+        return S3AsyncClient.builder()
+            .region(Region.of(s3Properties.region.static))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .build()
+    }
+}
