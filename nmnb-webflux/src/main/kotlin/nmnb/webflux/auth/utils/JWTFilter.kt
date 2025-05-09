4dbed7d8c00c08cee1e93ca3fa603b93c325196e
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:36:49 +0900

    feat: 인증 처리 및 사용자 인증 정보 설정을 위한 JWTFilter 생성

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/auth/utils/JWTFilter.kt
@@ -0,0 +1,53 @@
+package nmnb.webflux.auth.utils
+
+import nmnb.common.response.exception.GeneralException
+import nmnb.common.response.status.ErrorStatus
+import nmnb.r2dbc.user.R2dbcUserRepository
+import nmnb.webflux.auth.domain.CustomUserDetails
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
+@Component
+class JWTFilter(
+    private val jwtUtil: JwtUtil,
+    private val userRepository: R2dbcUserRepository,
+) : WebFilter {
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        val authorizationHeader = exchange.request.headers.getFirst(AUTHORIZATION_HEADER)
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            return chain.filter(exchange)
+        }
+
+        val parsedToken = authorizationHeader.substring(7)
+
+        return if (jwtUtil.isValidToken(parsedToken)) {
+            val username = jwtUtil.getId(parsedToken)
+
+            return userRepository.findByEmail(username)
+                .switchIfEmpty(Mono.error(GeneralException(ErrorStatus.USER_NOT_FOUND)))
+                .flatMap { user ->
+                    val userDetails = CustomUserDetails(user)
+
+                    val authenticationToken = UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.authorities,
+                    )
+
+                    chain.filter(exchange)
+                        .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authenticationToken))
+                }
+        } else {
+            Mono.error(GeneralException(ErrorStatus.AUTH_INVALID_TOKEN))
+        }
+    }
+    companion object {
+        const val AUTHORIZATION_HEADER = "Authorization"
+    }
+}
