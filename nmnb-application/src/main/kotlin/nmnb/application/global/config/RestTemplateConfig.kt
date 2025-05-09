0329da17f664d0e351a1ad9d64459467edb001c2
yel-m <coin6442@gmail.com>
2025-05-09 11:41:52 +0900

    chore : RestTemplateConfig 생성

---
+++ nmnb-application/src/main/kotlin/nmnb/application/global/config/RestTemplateConfig.kt
@@ -0,0 +1,21 @@
+package nmnb.application.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.FormHttpMessageConverter
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class RestTemplateConfig {
+    @Bean
+    fun restTemplate(): RestTemplate {
+        val restTemplate = RestTemplate()
+
+        val messageConverters: MutableList<HttpMessageConverter<*>> = ArrayList()
+        messageConverters.add(FormHttpMessageConverter())
+        restTemplate.messageConverters = messageConverters
+
+        return restTemplate
+    }
+}
