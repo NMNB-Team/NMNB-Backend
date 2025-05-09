6ecf6445baf64f58b770e169479c6371fe6f5069
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:44:19 +0900

    feat: WebFlux용 Security 설정 및 JWT 인증 필터 적용

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/config/SecurityConfig.kt
@@ -0,0 +1,53 @@
+package nmnb.webflux.config
+
+import nmnb.webflux.auth.utils.JWTFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository
+import org.springframework.web.server.WebFilter
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig(
+    private val jwtFilter: JWTFilter,
+) {
+
+    private val allowedUrl = arrayOf(
+        "/webjars/swagger-ui.html",
+        "/webjars/swagger-ui/**",
+        "webjars/v3/api-docs/**",
+        "/v1/api/upload",
+    )
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http.csrf { it.disable() }
+        http.httpBasic { httpBasicConfigurer -> httpBasicConfigurer.disable() }
+
+        http.formLogin { formLoginConfigurer -> formLoginConfigurer.disable() }
+
+        http
+            .securityContextRepository(WebSessionServerSecurityContextRepository())
+
+        http.authorizeExchange {
+            it.pathMatchers(*allowedUrl).permitAll()
+                .anyExchange().authenticated()
+        }
+
+        http.addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+
+        return http.build()
+    }
+
+    @Bean
+    fun headersWebFilter(): WebFilter {
+        return WebFilter { exchange, chain ->
+            exchange.response.headers.add("X-Frame-Options", "SAMEORIGIN")
+            chain.filter(exchange)
+        }
+    }
+}
