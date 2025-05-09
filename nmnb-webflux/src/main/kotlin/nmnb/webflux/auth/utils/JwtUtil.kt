fd232cc6704a7a5a1a58748c8666378fcf55cdeb
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:35:24 +0900

    feat: JWT 토큰 유효성 검사 및 사용자 정보 추출을 위한 JwtUtil 클래스 추가

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/auth/utils/JwtUtil.kt
@@ -0,0 +1,52 @@
+package nmnb.webflux.auth.utils
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.MalformedJwtException
+import io.jsonwebtoken.UnsupportedJwtException
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import nmnb.common.response.exception.GeneralException
+import nmnb.common.response.status.ErrorStatus
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import javax.crypto.SecretKey
+
+@Component
+class JwtUtil(
+    @Value("\${jwt.secret}") private val secret: String,
+) {
+    fun isValidToken(token: String): Boolean {
+        return try {
+            val claims = getClaims(token)
+            val now = java.util.Date()
+            val expiredDate = claims.body.expiration
+            expiredDate.after(now)
+        } catch (e: ExpiredJwtException) {
+            throw GeneralException(ErrorStatus.AUTH_EXPIRED_TOKEN)
+        } catch (e: SecurityException) {
+            throw GeneralException(ErrorStatus.AUTH_INVALID_TOKEN)
+        } catch (e: MalformedJwtException) {
+            throw GeneralException(ErrorStatus.AUTH_INVALID_TOKEN)
+        } catch (e: IllegalArgumentException) {
+            throw GeneralException(ErrorStatus.AUTH_INVALID_TOKEN)
+        } catch (e: UnsupportedJwtException) {
+            throw GeneralException(ErrorStatus.UNSUPPORTED_TOKEN)
+        }
+    }
+
+    fun getId(token: String): String {
+        return getClaims(token).body.get("userId", String::class.java)
+    }
+
+    private fun getClaims(token: String): Jws<Claims> {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token)
+    }
+
+    private fun getSigningKey(): SecretKey? {
+        val keyBytes = Decoders.BASE64.decode(secret)
+        return Keys.hmacShaKeyFor(keyBytes)
+    }
+}
