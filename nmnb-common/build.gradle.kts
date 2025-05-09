e364d4722ca36c20036b1890c0112178ab2ecab1
88dldl <chltnrgml0999@gmail.com>
2025-05-10 00:55:55 +0900

    refactor: spring-web으로 웹 의존성 경량화

--- nmnb-common/build.gradle.kts
+++ nmnb-common/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework:spring-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
