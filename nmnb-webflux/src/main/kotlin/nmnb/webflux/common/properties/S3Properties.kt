4c4175712317f94a7ef5ed9d12d5563e5c679bf8
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:13:39 +0900

    feat: S3 설정을 위한 ConfigurationProperties 클래스 추가

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/common/properties/S3Properties.kt
@@ -0,0 +1,25 @@
+package nmnb.webflux.common.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "cloud.aws")
+data class S3Properties(
+    var s3: S3 = S3(),
+    var credentials: Credentials = Credentials(),
+    var region: Region = Region(),
+) {
+    data class S3(
+        var bucket: String = "",
+    )
+
+    data class Credentials(
+        var accessKey: String = "",
+        var secretKey: String = "",
+    )
+
+    data class Region(
+        var static: String = "",
+    )
+}
