34daefb1cfbac7038adaf13e4cb63327b2ab0708
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:04:07 +0900

    chore: WebFlux Web 환경 설정

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/config/WebConfig.kt
@@ -0,0 +1,37 @@
+package nmnb.webflux.config
+
+import nmnb.webflux.handler.resolver.AuthUserArgumentResolver
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
+
+@Configuration
+class WebConfig(
+    private val authUserArgumentResolver: AuthUserArgumentResolver,
+) : WebFluxConfigurer {
+
+    @Bean
+    fun corsWebFilter(): CorsWebFilter {
+        val config = CorsConfiguration().apply {
+            addAllowedOrigin("*")
+            addAllowedMethod("*")
+            addAllowedHeader("*")
+            allowCredentials = false
+            maxAge = 6000
+        }
+
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
+
+        return CorsWebFilter(source)
+    }
+
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        configurer.addCustomResolver(authUserArgumentResolver)
+    }
+}
