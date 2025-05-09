2461810e597dd78a7ef726332d36c393c7beedf0
88dldl <chltnrgml0999@gmail.com>
2025-05-10 01:32:41 +0900

    chore: AWS SDK S3 V2 의존성 추가

--- nmnb-webflux/build.gradle.kts
+++ nmnb-webflux/build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
 
     // ConfigurationProperties
     annotationProcessor ("org.springframework.boot:spring-boot-configuration-processor")
+
+    // S3
+    implementation("software.amazon.awssdk:s3:2.19.1")
 }
 
 tasks.bootJar {
