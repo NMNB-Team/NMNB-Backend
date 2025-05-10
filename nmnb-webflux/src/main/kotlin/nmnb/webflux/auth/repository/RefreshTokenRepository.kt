109a9c3a1e8fd79fd9ab43859d111acb1e62534b
88dldl <chltnrgml0999@gmail.com>
2025-05-10 15:56:27 +0900

    remove: 미사용 리포지토리 삭제

--- nmnb-webflux/src/main/kotlin/nmnb/webflux/auth/repository/RefreshTokenRepository.kt
+++
@@ -1,6 +0,0 @@
-package nmnb.webflux.auth.repository
-
-import nmnb.webflux.auth.domain.RefreshToken
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
-
-interface RefreshTokenRepository : ReactiveCrudRepository<RefreshToken, String>
