2af8d38859c065f28947329dacc5cb094a64b6fd
yel-m <coin6442@gmail.com>
2025-05-14 15:14:53 +0900

    fix : domain 모듈 내 application 모듈 참조 방지를 위해 from 로직 컨버터로 이동

--- nmnb-application/src/main/kotlin/nmnb/application/global/auth/converter/StringToSocialTypeConverter.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/auth/converter/StringToSocialTypeConverter.kt
@@ -1,10 +1,15 @@
 package nmnb.application.global.auth.converter
 
+import nmnb.application.global.auth.exception.AuthException
+import nmnb.common.response.status.ErrorStatus
 import nmnb.domain.auth.SocialType
 import org.springframework.core.convert.converter.Converter
 import org.springframework.stereotype.Component
 
 @Component
 class StringToSocialTypeConverter : Converter<String, SocialType> {
-    override fun convert(source: String): SocialType = SocialType.from(source)
+    override fun convert(source: String): SocialType {
+        return SocialType.values().find { it.value.equals(source, ignoreCase = true) }
+            ?: throw AuthException(ErrorStatus.UNSUPPORTED_SOCIAL_TYPE)
+    }
 }
