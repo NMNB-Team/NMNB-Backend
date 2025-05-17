186719e380830761f74ed841c2622e4a3e02cbf2
88dldl <chltnrgml0999@gmail.com>
2025-05-18 02:29:31 +0900

    test: ApplicationContext 및 영상 업로드 테스트를 위한 MockBean 추가

--- nmnb-webflux/src/test/kotlin/nmnb/webflux/ControllerTestSupport.kt
+++ nmnb-webflux/src/test/kotlin/nmnb/webflux/ControllerTestSupport.kt
@@ -1,10 +1,15 @@
 package nmnb.webflux
 
+import nmnb.r2dbc.user.R2dbcUserRepository
 import nmnb.webflux.domain.post.controller.PostController
+import nmnb.webflux.domain.post.service.PostUploadService
+import nmnb.webflux.global.auth.utils.JwtTokenProvider
 import nmnb.webflux.global.config.SecurityConfig
 import nmnb.webflux.global.handler.resolver.AuthUserArgumentResolver
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Import
 import org.springframework.test.web.reactive.server.WebTestClient
 
@@ -13,4 +18,16 @@ import org.springframework.test.web.reactive.server.WebTestClient
 abstract class ControllerTestSupport {
     @Autowired
     protected lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    protected lateinit var context: ApplicationContext
+
+    @MockBean
+    protected lateinit var jwtTokenProvider: JwtTokenProvider
+
+    @MockBean
+    protected lateinit var userRepository: R2dbcUserRepository
+
+    @MockBean
+    protected lateinit var postUploadService: PostUploadService
 }
