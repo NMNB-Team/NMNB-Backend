595ab952700b003461432bab5b8dbd35cc1d4fb7
yel-m <coin6442@gmail.com>
2025-05-18 23:40:53 +0900

    chore : ExtractToken 리졸버 WebConfig에 등록

--- nmnb-application/src/main/kotlin/nmnb/application/global/config/WebConfig.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/config/WebConfig.kt
@@ -1,13 +1,17 @@
 package nmnb.application.global.config
 
 import nmnb.application.global.auth.converter.StringToSocialTypeConverter
+import nmnb.application.global.auth.generator.ExtractTokenArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    private val extractTokenArgumentResolver: ExtractTokenArgumentResolver,
+) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
@@ -19,5 +23,9 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addFormatters(registry: FormatterRegistry) {
         registry.addConverter(StringToSocialTypeConverter())
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(extractTokenArgumentResolver)
     }
 }
