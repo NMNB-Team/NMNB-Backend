8a7907d71e8e5a6242267705366a500c62a68572
yel-m <coin6442@gmail.com>
2025-05-21 13:02:49 +0900

    refactor : 애플리케이션 모듈의 S3Properties webflux 모듈의 S3Properties 경로와 통일

--- nmnb-application/src/main/kotlin/nmnb/application/global/common/properties/S3Properties.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/common/properties/S3Properties.kt
@@ -1,4 +1,4 @@
-package nmnb.application.global.config
+package nmnb.application.global.common.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
