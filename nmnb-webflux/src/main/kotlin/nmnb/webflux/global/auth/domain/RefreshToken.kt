bd12104064952897888d834fed03fe7ac41cd452
88dldl <chltnrgml0999@gmail.com>
2025-05-21 09:55:22 +0900

    chore: 불필요한 `@Table` 삭제

--- nmnb-webflux/src/main/kotlin/nmnb/webflux/global/auth/domain/RefreshToken.kt
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/global/auth/domain/RefreshToken.kt
@@ -2,9 +2,7 @@ package nmnb.webflux.global.auth.domain
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
-import org.springframework.data.relational.core.mapping.Table
 
-@Table("refresh_token")
 @RedisHash("refresh_token")
 data class RefreshToken(
     @Id
