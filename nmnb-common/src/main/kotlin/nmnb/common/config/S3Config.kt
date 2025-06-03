9ae6c768977c7eb1314961cf14f1f3fcab0398c3
88dldl <chltnrgml0999@gmail.com>
2025-06-03 19:51:18 +0900

    feat: S3Client 빈 등록 및 AWS 자격 증명 설정 추가

--- nmnb-common/src/main/kotlin/nmnb/common/config/S3Config.kt
+++ nmnb-common/src/main/kotlin/nmnb/common/config/S3Config.kt
@@ -7,11 +7,25 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3AsyncClient
+import software.amazon.awssdk.services.s3.S3Client
 
 @Configuration
 class S3Config(
     private val s3Properties: S3Properties,
 ) {
+    @Bean
+    fun s3Client(): S3Client {
+        val credentials = AwsBasicCredentials.create(
+            s3Properties.credentials.accessKey,
+            s3Properties.credentials.secretKey,
+        )
+
+        return S3Client.builder()
+            .region(Region.of(s3Properties.region.static))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .build()
+    }
+
     @Bean
     fun s3AsyncClient(): S3AsyncClient {
         val credentials = AwsBasicCredentials.create(s3Properties.credentials.accessKey, s3Properties.credentials.secretKey)
