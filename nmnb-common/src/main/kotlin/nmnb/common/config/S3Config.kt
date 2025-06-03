c4f51ab12712b52260fa01324a1d1db8d9df4182
88dldl <chltnrgml0999@gmail.com>
2025-06-03 19:52:09 +0900

    feat: url 조회를 위한 S3Presigner 빈 등록

--- nmnb-common/src/main/kotlin/nmnb/common/config/S3Config.kt
+++ nmnb-common/src/main/kotlin/nmnb/common/config/S3Config.kt
@@ -8,6 +8,7 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
 
 @Configuration
 class S3Config(
@@ -30,6 +31,18 @@ class S3Config(
     fun s3AsyncClient(): S3AsyncClient {
         val credentials = AwsBasicCredentials.create(s3Properties.credentials.accessKey, s3Properties.credentials.secretKey)
         return S3AsyncClient.builder()
+            .region(Region.of(s3Properties.region.static))
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .build()
+    }
+
+    @Bean
+    fun s3Presigner(): S3Presigner {
+        val credentials = AwsBasicCredentials.create(
+            s3Properties.credentials.accessKey,
+            s3Properties.credentials.secretKey,
+        )
+        return S3Presigner.builder()
             .region(Region.of(s3Properties.region.static))
             .credentialsProvider(StaticCredentialsProvider.create(credentials))
             .build()
