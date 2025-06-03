41c337ab39567fc8c9a3da61274b6935b321c4be
88dldl <chltnrgml0999@gmail.com>
2025-06-03 19:57:14 +0900

    feat: 프로필 이미지 생성 및 URL 응답 기능 구현

--- nmnb-application/src/main/kotlin/nmnb/application/global/infrastructure/external/s3/S3Service.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/infrastructure/external/s3/S3Service.kt
@@ -1,17 +1,46 @@
 package nmnb.application.global.infrastructure.external.s3
 
 import nmnb.common.properties.S3Properties
+import nmnb.common.response.exception.S3Exception
+import nmnb.common.response.status.ErrorStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import software.amazon.awssdk.core.exception.SdkClientException
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
 import java.time.Duration
 
 @Service
 class S3Service(
+    private val s3Client: S3Client,
     private val s3Presigner: S3Presigner,
     private val s3Properties: S3Properties,
 ) {
+    fun uploadProfileImage(fileName: String, profileImage: MultipartFile): String {
+        val s3Key = generateS3Key(PROFILE_IMAGE_PATH, fileName)
+        val request = createRequest(s3Key, profileImage)
+
+        try {
+            s3Client.putObject(request, RequestBody.fromInputStream(profileImage.inputStream, profileImage.size))
+        } catch (e: SdkClientException) {
+            throw S3Exception(ErrorStatus.S3_UPLOAD_PROFILE_IMAGE_FAILED)
+        }
+
+        return generatePresignedUrl(s3Key)
+    }
+
+    private fun createRequest(s3Key: String, profileImage: MultipartFile): PutObjectRequest =
+        PutObjectRequest.builder()
+            .bucket(s3Properties.s3.bucket)
+            .key(s3Key)
+            .contentType(profileImage.contentType)
+            .contentLength(profileImage.size)
+            .build()
+
     private fun generatePresignedUrl(key: String): String {
         val getObjectRequest = GetObjectRequest.builder()
             .bucket(s3Properties.s3.bucket)
@@ -26,7 +55,11 @@ class S3Service(
         return s3Presigner.presignGetObject(presignRequest).url().toString()
     }
 
+    private fun generateS3Key(folder: String, fileName: String) =
+        "$folder/$fileName"
+
     companion object {
         private val PRESIGNED_URL_EXPIRATION: Duration = Duration.ofHours(1)
+        private const val PROFILE_IMAGE_PATH = "profile"
     }
 }
