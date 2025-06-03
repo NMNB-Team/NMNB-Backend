3ebc86a274a48bb7f68c32b8012adcff223bb381
88dldl <chltnrgml0999@gmail.com>
2025-06-03 20:36:37 +0900

    feat: 사용자 마이페이지 수정 API 구현

--- nmnb-application/src/main/kotlin/nmnb/application/domain/user/controller/UserController.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/domain/user/controller/UserController.kt
@@ -2,9 +2,11 @@ package nmnb.application.domain.user.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
+import nmnb.application.domain.user.controller.dto.request.EditProfileRequest
 import nmnb.application.domain.user.controller.dto.request.UserPetRegistrationRequest
 import nmnb.application.domain.user.service.UserService
 import nmnb.application.domain.user.service.dto.response.UserProfileResponse
@@ -14,11 +16,14 @@ import nmnb.application.global.auth.generator.annotation.TokenApiResponse
 import nmnb.common.response.base.BaseResponse
 import nmnb.common.response.status.SuccessStatus
 import nmnb.domain.user.User
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @Tag(name = "User 👤", description = "사용자 관련 API")
@@ -32,6 +37,30 @@ class UserController(
     @GetMapping("/profile")
     fun getProfile(@Parameter(name = "user", hidden = true) @AuthUser user: User): BaseResponse<UserProfileResponse> {
         return BaseResponse.onSuccess(SuccessStatus.OK, userService.getProfile(user))
+    }
+
+    @Operation(summary = "사용자 마이페이지 수정 API", description = "사용자의 마이페이지를 수정합니다._숙희")
+    @ApiResponses(ApiResponse(responseCode = "COMMON200", description = "성공입니다."))
+    @PatchMapping("/profile", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun editProfile(
+        @Parameter(name = "user", hidden = true) @AuthUser user: User,
+        @RequestPart(value = "request") @Parameter(
+            description = """다음과 같은 JSON 형태의 요청을 입력해야 합니다:
+                {
+                    "petName": "petName"
+                }
+                """,
+            schema = Schema(
+                type = "string",
+                format = "binary",
+            ),
+        ) request: EditProfileRequest,
+        @RequestPart(value = "profileImage", required = false) profileImage: MultipartFile?,
+    ): BaseResponse<Any> {
+        return BaseResponse.onSuccess(
+            SuccessStatus.OK,
+            userService.editProfile(user, request.toServiceRequest(), profileImage),
+        )
     }
 
     @Operation(summary = "반려견 이름 등록 API", description = "가입하는 유저가 반려견의 이름을 등록합니다. 유저의 petOwnershipStatus가 `HAS_PET`으로 설정됩니다._예림")
