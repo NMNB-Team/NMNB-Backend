450fba97226580fa788b7d9b01e31498474a4f8a
88dldl <chltnrgml0999@gmail.com>
2025-06-03 21:04:10 +0900

    chore: 사용자 마이페이지 수정 관련 Swagger 응답 코드 추가

--- nmnb-application/src/main/kotlin/nmnb/application/domain/user/controller/UserController.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/domain/user/controller/UserController.kt
@@ -41,7 +41,13 @@ class UserController(
     }
 
     @Operation(summary = "사용자 마이페이지 수정 API", description = "사용자의 마이페이지를 수정합니다._숙희")
-    @ApiResponses(ApiResponse(responseCode = "COMMON200", description = "성공입니다."))
+    @ApiResponses(
+        ApiResponse(responseCode = "COMMON200", description = "성공입니다."),
+        ApiResponse(responseCode = "PET400", description = "반려동물 이름은 필수입니다."),
+        ApiResponse(responseCode = "PET401", description = "반려동물 등록이 필요합니다."),
+        ApiResponse(responseCode = "PET402", description = "반려동물 상태 값이 유효하지 않습니다."),
+        ApiResponse(responseCode = "S3501", description = "프로필 이미지 업로드 중 오류가 발생했습니다."),
+    )
     @PostMapping("/profile", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun editProfile(
         @Parameter(name = "user", hidden = true) @AuthUser user: User,
