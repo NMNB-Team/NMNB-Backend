571eb644f1b8d915482a20fbdf66e0937a2ca2d0
88dldl <chltnrgml0999@gmail.com>
2025-06-03 19:59:46 +0900

    feat: 프로필 이미지, 반려동물 이름 프로필 수정 기능 구현

--- nmnb-application/src/main/kotlin/nmnb/application/domain/user/service/UserService.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/domain/user/service/UserService.kt
@@ -1,12 +1,15 @@
 package nmnb.application.domain.user.service
 
+import nmnb.application.domain.user.service.dto.request.EditProfileServiceRequest
 import nmnb.application.domain.user.service.dto.request.UserPetRegistrationServiceRequest
 import nmnb.application.domain.user.service.dto.response.UserProfileResponse
 import nmnb.application.domain.user.service.dto.response.UserStatusResponse
 import nmnb.domain.user.User
+import org.springframework.web.multipart.MultipartFile
 
 interface UserService {
     fun getProfile(user: User): UserProfileResponse
     fun registerWithPetName(user: User, request: UserPetRegistrationServiceRequest): UserStatusResponse
     fun registerWithoutPet(user: User): UserStatusResponse
+    fun editProfile(user: User, request: EditProfileServiceRequest, profileImage: MultipartFile? = null)
 }
--- nmnb-application/src/main/kotlin/nmnb/application/domain/user/service/UserServiceImpl.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/domain/user/service/UserServiceImpl.kt
@@ -1,19 +1,25 @@
 package nmnb.application.domain.user.service
 
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils
+import nmnb.application.domain.user.service.dto.request.EditProfileServiceRequest
 import nmnb.application.domain.user.service.dto.request.UserPetRegistrationServiceRequest
 import nmnb.application.domain.user.service.dto.response.UserProfileResponse
 import nmnb.application.domain.user.service.dto.response.UserStatusResponse
+import nmnb.application.global.infrastructure.external.s3.S3Service
 import nmnb.common.domain.PetOwnershipStatus
 import nmnb.common.domain.SignUpStatus
 import nmnb.domain.user.User
 import nmnb.domain.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDateTime
 
 @Service
 @Transactional(readOnly = true)
 class UserServiceImpl(
     private val userRepository: UserRepository,
+    private val s3Service: S3Service,
 ) : UserService {
 
     override fun getProfile(user: User): UserProfileResponse {
@@ -36,5 +42,30 @@ class UserServiceImpl(
     private fun completeRegistration(user: User, petOwnershipStatus: PetOwnershipStatus) {
         user.updatePetOwnershipStatus(petOwnershipStatus)
         user.updateSignUpStatus(SignUpStatus.COMPLETE)
+    }
+
+    @Transactional
+    override fun editProfile(user: User, request: EditProfileServiceRequest, profileImage: MultipartFile?) {
+        val profileImageUrl = profileImage?.let {
+            val timeStamp = LocalDateTime.now().toString()
+            val originalFileName = it.originalFilename.toString()
+
+            val fileName = generateFileName(timeStamp, originalFileName)
+            s3Service.uploadProfileImage(fileName, it)
+        }
+
+        user.updateProfile(request.petName, profileImageUrl)
+    }
+    private fun generateFileName(date: String, name: String): String {
+        return date + "_" + NanoIdUtils.randomNanoId(
+            NanoIdUtils.DEFAULT_NUMBER_GENERATOR,
+            CUSTOM_STRING.toCharArray(),
+            ID_LENGTH,
+        ) + "-" + name
+    }
+
+    companion object {
+        private const val ID_LENGTH: Int = 9
+        private const val CUSTOM_STRING = "1234567890"
     }
 }
