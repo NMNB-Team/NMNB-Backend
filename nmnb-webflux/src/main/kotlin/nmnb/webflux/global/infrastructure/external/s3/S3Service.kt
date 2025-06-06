cc2c691e01814f78e91fbdbc8238a61035650fa5
88dldl <chltnrgml0999@gmail.com>
2025-06-06 19:57:19 +0900

    refactor: S3Utils 클래스 리팩토링에 따른 관련 메서드 수정 및 호출 방식 변경

--- nmnb-webflux/src/main/kotlin/nmnb/webflux/global/infrastructure/external/s3/S3Service.kt
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/global/infrastructure/external/s3/S3Service.kt
@@ -4,54 +4,65 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.future.await
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.withContext
+import nmnb.common.domain.AccessStrategy
 import nmnb.common.properties.S3Properties
 import nmnb.common.response.exception.PostException
 import nmnb.common.response.status.ErrorStatus
-import nmnb.common.utils.S3KeyUtils.generateS3Key
+import nmnb.common.utils.S3Utils
 import org.springframework.http.codec.multipart.FilePart
 import org.springframework.stereotype.Component
 import software.amazon.awssdk.core.async.AsyncRequestBody
 import software.amazon.awssdk.core.async.AsyncResponseTransformer
 import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
-import software.amazon.awssdk.services.s3.presigner.S3Presigner
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
 import java.io.File
 import java.nio.file.Files
-import java.time.Duration
 import java.util.UUID
 
 @Component
 class S3Service(
     private val s3AsyncClient: S3AsyncClient,
-    private val s3Presigner: S3Presigner,
     private val s3Properties: S3Properties,
+    private val s3Utils: S3Utils,
 ) {
 
-    suspend fun uploadVideo(baseFileName: String, filePart: FilePart, duration: Int): String = withContext(Dispatchers.IO) {
-        val s3Key = generateS3Key(VIDEO_FOLDER, baseFileName)
-        val tempFile = createTempFile(filePart)
-        try {
-            uploadToS3(s3Key, tempFile, metadata = mapOf("duration" to duration.toString()))
-            return@withContext generatePresignedUrl(s3Key)
-        } finally {
-            tempFile.delete()
+    suspend fun uploadVideo(
+        baseFileName: String,
+        filePart: FilePart,
+        duration: Int,
+        accessStrategy: AccessStrategy,
+    ): String =
+        withContext(Dispatchers.IO) {
+            val s3Key = s3Utils.generateS3Key(VIDEO_FOLDER, baseFileName)
+            val tempFile = createTempFile(filePart)
+            try {
+                uploadToS3(
+                    s3Key,
+                    tempFile,
+                    accessStrategy.cannedAcl,
+                    metadata = mapOf("duration" to duration.toString()),
+                )
+                return@withContext s3Utils.generateUrl(s3Key, accessStrategy)
+            } finally {
+                tempFile.delete()
+            }
         }
-    }
 
-    suspend fun uploadThumbnail(baseFileName: String, file: File): String = withContext(Dispatchers.IO) {
-        val s3Key = generateS3Key(THUMBNAIL_FOLDER, baseFileName)
-        uploadToS3(s3Key, file)
-        generatePresignedUrl(s3Key)
-    }
+    suspend fun uploadThumbnail(baseFileName: String, file: File, access: AccessStrategy): String =
+        withContext(Dispatchers.IO) {
+            val s3Key = s3Utils.generateS3Key(THUMBNAIL_FOLDER, baseFileName)
+            uploadToS3(s3Key, file)
+            s3Utils.generateUrl(s3Key, access)
+        }
 
     suspend fun download(fileName: String): File = withContext(Dispatchers.IO) {
         val tempFile = File(System.getProperty("java.io.tmpdir"), "video-${UUID.randomUUID()}.tmp")
         try {
             val request = GetObjectRequest.builder()
                 .bucket(s3Properties.s3.bucket)
-                .key(generateS3Key(VIDEO_FOLDER, fileName))
+                .key(s3Utils.generateS3Key(VIDEO_FOLDER, fileName))
                 .build()
 
             s3AsyncClient.getObject(request, AsyncResponseTransformer.toFile(tempFile.toPath())).await()
@@ -92,20 +103,6 @@ class S3Service(
 
         val requestBody = AsyncRequestBody.fromFile(file.toPath())
         s3AsyncClient.putObject(request, requestBody).await()
-    }
-
-    private fun generatePresignedUrl(key: String): String {
-        val getObjectRequest = GetObjectRequest.builder()
-            .bucket(s3Properties.s3.bucket)
-            .key(key)
-            .build()
-
-        val presignRequest = GetObjectPresignRequest.builder()
-            .getObjectRequest(getObjectRequest)
-            .signatureDuration(Duration.ofHours(1))
-            .build()
-
-        return s3Presigner.presignGetObject(presignRequest).url().toString()
     }
 
     companion object {
