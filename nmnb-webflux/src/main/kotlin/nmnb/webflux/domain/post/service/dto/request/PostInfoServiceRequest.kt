3884f3761856ce9aed300e348ee6a0f31958a046
88dldl <chltnrgml0999@gmail.com>
2025-06-06 19:49:42 +0900

    feat: 게시글 작성 요청에 accessStrategy 필드 추가

--- nmnb-webflux/src/main/kotlin/nmnb/webflux/domain/post/service/dto/request/PostInfoServiceRequest.kt
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/domain/post/service/dto/request/PostInfoServiceRequest.kt
@@ -1,6 +1,9 @@
 package nmnb.webflux.domain.post.service.dto.request
 
+import nmnb.common.domain.AccessStrategy
+
 data class PostInfoServiceRequest(
     val description: String?,
     val duration: Int,
+    val accessStrategy: AccessStrategy,
 )
