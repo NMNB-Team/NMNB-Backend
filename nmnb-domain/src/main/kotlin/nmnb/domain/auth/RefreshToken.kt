db70f9540f84ffd887e76bb345d8cca4c5be1add
88dldl <chltnrgml0999@gmail.com>
2025-06-10 05:06:43 +0900

    refactor: RefreshToken 도메인 필드 추가(deviceId, timestamp) 및 구조 변경

--- nmnb-domain/src/main/kotlin/nmnb/domain/auth/RefreshToken.kt
+++ nmnb-domain/src/main/kotlin/nmnb/domain/auth/RefreshToken.kt
@@ -2,14 +2,18 @@ package nmnb.domain.auth
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import java.time.LocalDateTime
 
 @RedisHash("refresh_token")
 data class RefreshToken(
     @Id
-    val email: String,
-
+    val id: String,
+    var email: String,
     var refreshToken: String,
-) {
+    var deviceId: String,
+    var timeStamp: LocalDateTime,
+
+    ) {
 
     fun update(newToken: String) {
         this.refreshToken = newToken
