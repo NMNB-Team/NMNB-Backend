d64a4481f69c76b3f4fc33fc2edf7380e03944a1
88dldl <chltnrgml0999@gmail.com>
2025-06-18 20:01:05 +0900

    refactor: Token 헤더에 따라 AccessToken, RefreshToken 어노테이션 분리

--- nmnb-application/src/main/kotlin/nmnb/application/global/auth/generator/annotation/ExtractRefreshToken.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/auth/generator/annotation/ExtractRefreshToken.kt
@@ -2,4 +2,4 @@ package nmnb.application.global.auth.generator.annotation
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.VALUE_PARAMETER)
-annotation class ExtractToken
+annotation class ExtractRefreshToken
