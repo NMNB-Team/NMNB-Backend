1185c9d26403f213ea5b8a10facde50ae9a27062
88dldl <chltnrgml0999@gmail.com>
2025-06-19 16:01:28 +0900

    test: RefreshToken 삭제 기능 테스트 코드 작성

--- nmnb-application/src/test/kotlin/nmnb/application/global/auth/service/RefreshTokenServiceTest.kt
+++ nmnb-application/src/test/kotlin/nmnb/application/global/auth/service/RefreshTokenServiceTest.kt
@@ -1,6 +1,7 @@
 package nmnb.application.global.auth.service
 
 import nmnb.application.global.common.utils.DeviceIdUtils
+import nmnb.application.global.infrastructure.security.JwtProvider
 import nmnb.domain.auth.RefreshToken
 import nmnb.domain.auth.repository.RefreshTokenRepository
 import org.assertj.core.api.Assertions.assertThat
@@ -30,6 +31,9 @@ class RefreshTokenServiceTest {
 
     @Mock
     private lateinit var refreshTokenRepository: RefreshTokenRepository
+
+    @Mock
+    private lateinit var jwtProvider: JwtProvider
 
     @Test
     @DisplayName("한 계정에 저장된 RefreshToken 수가 제한보다 작으면 아무 것도 삭제하지 않는다")
@@ -134,5 +138,25 @@ class RefreshTokenServiceTest {
         assertEquals(deviceId, savedToken.deviceId)
         assertEquals(newRefreshToken, savedToken.refreshToken)
         assertThat(savedToken.timeStamp).isNotNull
+    }
+
+    @Test
+    @DisplayName("RefreshToken이 유효하면 저장소에서 삭제한다")
+    fun deleteRefreshToken() {
+        // given
+        val email = "email@email.com"
+        val deviceId = "device1"
+        val refreshToken = "refresh-token"
+        val id = DeviceIdUtils.deviceIdFormatter(email, deviceId)
+        val token = RefreshToken.fixture(email, refreshToken, LocalDateTime.now(), deviceId)
+
+        whenever(jwtProvider.getEmailWithValidation(any())).thenReturn(email)
+        whenever(refreshTokenRepository.findById(id)).thenReturn(Optional.of(token))
+
+        // when
+        refreshTokenService.deleteRefreshToken(id, refreshToken)
+
+        // then
+        verify(refreshTokenRepository).deleteById(id)
     }
 }
