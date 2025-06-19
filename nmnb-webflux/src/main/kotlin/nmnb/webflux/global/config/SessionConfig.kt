9ed1ca6e351c5a0aefe853f96cd725b5359083d4
88dldl <chltnrgml0999@gmail.com>
2025-06-19 17:11:06 +0900

    feat: WebFlux 환경에서 Redis 세션 활성화를 위한 설정 추가

---
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/global/config/SessionConfig.kt
@@ -0,0 +1,8 @@
+package nmnb.webflux.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.session.config.annotation.web.server.EnableSpringWebSession
+
+@EnableSpringWebSession
+@Configuration
+class SessionConfig
