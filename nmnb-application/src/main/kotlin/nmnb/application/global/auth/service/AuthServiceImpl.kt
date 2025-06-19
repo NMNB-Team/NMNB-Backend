b5d6e084c71840087f7947333b9cb04da728df72
88dldl <chltnrgml0999@gmail.com>
2025-06-19 16:04:08 +0900

    refactor: 서비스 클래스에 읽기 전용 트랜잭션 적용

--- nmnb-application/src/main/kotlin/nmnb/application/global/auth/service/AuthServiceImpl.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/auth/service/AuthServiceImpl.kt
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 
+@Transactional(readOnly = true)
 @Service
 class AuthServiceImpl(
     private val oAuthClientComposite: OAuthClientComposite,
