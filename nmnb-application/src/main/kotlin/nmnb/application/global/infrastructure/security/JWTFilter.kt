dfe365d31f7d6cb769bf224e223336168b0899a0
yel-m <coin6442@gmail.com>
2025-07-18 17:21:25 +0900

    fix : 액세스 토큰이 없을 시 예외 처리 추가 및 필터 단에서 에러 응답을 반환하도록 수정

--- nmnb-application/src/main/kotlin/nmnb/application/global/infrastructure/security/JWTFilter.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/infrastructure/security/JWTFilter.kt
@@ -8,6 +8,7 @@ import nmnb.common.response.exception.AuthException
 import nmnb.common.response.exception.GeneralException
 import nmnb.common.response.status.ErrorStatus
 import nmnb.common.utils.HeaderConstants.ACCESS_TOKEN_HEADER
+import nmnb.common.utils.ResponseUtils
 import nmnb.domain.user.repository.UserRepository
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -20,6 +21,7 @@ class JWTFilter(
     private val jwtProvider: JwtProvider,
     private val userRepository: UserRepository,
     private val blacklistService: BlacklistService,
+    private val responseUtils: ResponseUtils,
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(
@@ -28,32 +30,38 @@ class JWTFilter(
         filterChain: FilterChain,
     ) {
         val accessToken = request.getHeader(ACCESS_TOKEN_HEADER)
-        if (accessToken != null) {
-            try {
-                if (blacklistService.isBlacklisted(accessToken)) {
-                    throw AuthException(ErrorStatus.TOKEN_LOGGED_OUT)
-                }
 
-                val email = jwtProvider.getEmailWithValidation(accessToken)
-                val user = userRepository.findByEmail(email)
-                    ?: throw AuthException(ErrorStatus.USER_NOT_FOUND)
+        if (accessToken == null) {
+            responseUtils.sendErrorResponse(response, ErrorStatus.AUTH_ACCESS_TOKEN_MISSING.getReasonHttpStatus())
+            return
+        }
 
-                val userDetails = CustomUserDetails(user)
-
-                val authentication = UsernamePasswordAuthenticationToken(
-                    userDetails,
-                    null,
-                    userDetails.authorities,
-                ).apply {
-                    details = WebAuthenticationDetailsSource().buildDetails(request)
-                }
-
-                SecurityContextHolder.getContext().authentication = authentication
-            } catch (e: AuthException) {
-                throw e
-            } catch (e: GeneralException) {
-                throw AuthException(ErrorStatus.UNAUTHORIZED)
+        try {
+            if (blacklistService.isBlacklisted(accessToken)) {
+                throw AuthException(ErrorStatus.TOKEN_LOGGED_OUT)
             }
+
+            val email = jwtProvider.getEmailWithValidation(accessToken)
+            val user = userRepository.findByEmail(email)
+                ?: throw AuthException(ErrorStatus.USER_NOT_FOUND)
+
+            val userDetails = CustomUserDetails(user)
+
+            val authentication = UsernamePasswordAuthenticationToken(
+                userDetails,
+                null,
+                userDetails.authorities,
+            ).apply {
+                details = WebAuthenticationDetailsSource().buildDetails(request)
+            }
+
+            SecurityContextHolder.getContext().authentication = authentication
+        } catch (e: GeneralException) {
+            responseUtils.sendErrorResponse(response, e.getErrorReasonHttpStatus())
+            return
+        } catch (e: Exception) {
+            responseUtils.sendErrorResponse(response, ErrorStatus.UNAUTHORIZED.getReasonHttpStatus())
+            return
         }
 
         filterChain.doFilter(request, response)
