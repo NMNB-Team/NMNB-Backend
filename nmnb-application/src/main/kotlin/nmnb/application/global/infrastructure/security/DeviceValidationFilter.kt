4bbfeb250cd4d7e972763a60e88740b97fe505f1
yel-m <coin6442@gmail.com>
2025-07-18 17:14:39 +0900

    feat : 에러 응답을 반환하는 ResponseUtils 클래스 생성 및 적용

--- nmnb-application/src/main/kotlin/nmnb/application/global/infrastructure/security/DeviceValidationFilter.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/global/infrastructure/security/DeviceValidationFilter.kt
@@ -1,22 +1,20 @@
 package nmnb.application.global.infrastructure.security
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import nmnb.common.response.base.BaseResponse
-import nmnb.common.response.exception.AuthException
 import nmnb.common.response.status.ErrorStatus
 import nmnb.common.utils.HeaderConstants.ACCESS_TOKEN_HEADER
 import nmnb.common.utils.HeaderConstants.DEVICE_ID_HEADER
 import nmnb.common.utils.JwtConstants.DEVICE_ID_CLAIM_KEY
+import nmnb.common.utils.ResponseUtils
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class DeviceValidationFilter(
     private val jwtProvider: JwtProvider,
-    private val objectMapper: ObjectMapper,
+    private val responseUtils: ResponseUtils,
 ) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
@@ -30,28 +28,15 @@ class DeviceValidationFilter(
                 val deviceIdInRequest = request.getHeader(DEVICE_ID_HEADER)
 
                 if (deviceIdInToken == null || deviceIdInRequest == null || deviceIdInToken != deviceIdInRequest) {
-                    sendErrorResponse(response, ErrorStatus.DEVICE_ID_MISMATCH)
+                    responseUtils.sendErrorResponse(response, ErrorStatus.DEVICE_ID_MISMATCH.getReasonHttpStatus())
                     return
                 }
             } catch (e: Exception) {
-                sendErrorResponse(response, ErrorStatus.UNAUTHORIZED)
+                responseUtils.sendErrorResponse(response, ErrorStatus.UNAUTHORIZED.getReasonHttpStatus())
                 return
             }
         }
 
         filterChain.doFilter(request, response)
-    }
-
-    private fun sendErrorResponse(response: HttpServletResponse, errorStatus: ErrorStatus) {
-        response.status = errorStatus.httpStatus!!.value()
-        response.contentType = "application/json;charset=UTF-8"
-
-        val errorBody = BaseResponse.onFailure(
-            errorStatus.code,
-            errorStatus.message,
-            null
-        )
-
-        response.writer.write(objectMapper.writeValueAsString(errorBody))
     }
 }
