5d08d0d8766695e9f08268eb8baf7e0f5febc970
yel-m <coin6442@gmail.com>
2025-07-19 00:11:10 +0900

    fix : JwtFilter 예외 처리 추가

--- nmnb-webflux/src/main/kotlin/nmnb/webflux/global/infrastructure/security/JWTFilter.kt
+++ nmnb-webflux/src/main/kotlin/nmnb/webflux/global/infrastructure/security/JWTFilter.kt
@@ -6,6 +6,7 @@ import nmnb.common.response.status.ErrorStatus
 import nmnb.common.utils.HeaderConstants.ACCESS_TOKEN_HEADER
 import nmnb.r2dbc.user.R2dbcUserRepository
 import nmnb.webflux.global.auth.domain.CustomUserDetails
+import nmnb.webflux.global.utils.ResponseUtils
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.stereotype.Component
@@ -19,32 +20,50 @@ class JWTFilter(
     private val jwtProvider: JwtProvider,
     private val userRepository: R2dbcUserRepository,
     private val blacklistService: BlacklistService,
+    private val responseUtils: ResponseUtils,
 ) : WebFilter {
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
         val accessToken =
             exchange.request.headers.getFirst(ACCESS_TOKEN_HEADER) ?: return chain.filter(exchange)
-        if (!jwtProvider.isValidToken(accessToken)) return Mono.error(GeneralException(ErrorStatus.AUTH_INVALID_TOKEN))
 
-        return blacklistService.isBlacklisted(accessToken).flatMap { isBlacklisted ->
-            if (isBlacklisted) {
-                return@flatMap Mono.error(AuthException(ErrorStatus.TOKEN_LOGGED_OUT))
-            }
-            val username = jwtProvider.getEmail(accessToken)
+        return try {
+            if (!jwtProvider.isValidToken(accessToken)) return Mono.error(GeneralException(ErrorStatus.AUTH_INVALID_TOKEN))
 
-            return@flatMap userRepository.findByEmail(username)
-                .switchIfEmpty(Mono.error(GeneralException(ErrorStatus.USER_NOT_FOUND)))
-                .flatMap { user ->
-                    val userDetails = CustomUserDetails(user)
-
-                    val authenticationToken = UsernamePasswordAuthenticationToken(
-                        userDetails,
-                        null,
-                        userDetails.authorities,
-                    )
-
-                    chain.filter(exchange)
-                        .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authenticationToken))
+            blacklistService.isBlacklisted(accessToken).flatMap { isBlacklisted ->
+                if (isBlacklisted) {
+                    return@flatMap Mono.error(AuthException(ErrorStatus.TOKEN_LOGGED_OUT))
                 }
+                val username = jwtProvider.getEmail(accessToken)
+
+                return@flatMap userRepository.findByEmail(username)
+                    .switchIfEmpty(Mono.error(GeneralException(ErrorStatus.USER_NOT_FOUND)))
+                    .flatMap { user ->
+                        val userDetails = CustomUserDetails(user)
+
+                        val authenticationToken = UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.authorities,
+                        )
+
+                        chain.filter(exchange)
+                            .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authenticationToken))
+                    }
+            }
+
+        } catch (e: Exception) {
+            Mono.error(e)
+        }.onErrorResume { throwable ->
+            when (throwable) {
+                is GeneralException -> responseUtils.sendErrorResponse(
+                    exchange,
+                    throwable.getErrorReasonHttpStatus()
+                )
+                else -> responseUtils.sendErrorResponse(
+                    exchange,
+                    ErrorStatus.UNAUTHORIZED.getReasonHttpStatus()
+                )
+            }
         }
     }
 }
