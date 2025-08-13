03227ddef766fd2232021fea49d3d738bc323873
88dldl <chltnrgml0999@gmail.com>
2025-08-13 22:55:14 +0900

    feat: post/user 서비스용 DTO 변환 메서드 추가

--- nmnb-application/src/main/kotlin/nmnb/application/domain/report/controller/request/ReportRequest.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/domain/report/controller/request/ReportRequest.kt
@@ -1,5 +1,9 @@
 package nmnb.application.domain.report.controller.request
 
+import nmnb.application.domain.report.service.dto.request.PostReportServiceRequest
+import nmnb.application.domain.report.service.dto.request.UserReportServiceRequest
+import nmnb.common.response.exception.ReportException
+import nmnb.common.response.status.ErrorStatus
 import nmnb.domain.report.ContentType
 import nmnb.domain.report.ReportType
 
@@ -7,4 +11,13 @@ data class ReportRequest(
     val type: ReportType,
     val targetId: String,
     val contentType: ContentType,
-)
+) {
+    fun toPostServiceRequest(): PostReportServiceRequest {
+        val id = targetId.toLongOrNull() ?: throw ReportException(ErrorStatus.INVALID_POST_TARGET_ID)
+        return PostReportServiceRequest(id, contentType)
+    }
+
+    fun toUserServiceRequest(): UserReportServiceRequest {
+        return UserReportServiceRequest(targetId, contentType)
+    }
+}
