918ebce017a533d4bdccd4330a6949de58c8464a
88dldl <chltnrgml0999@gmail.com>
2025-08-13 23:58:02 +0900

    feat: 공통 신고 로직 반영 및 중복/본인 신고 점증 로직 추가

--- nmnb-application/src/main/kotlin/nmnb/application/domain/report/service/PostReportService.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/domain/report/service/PostReportService.kt
@@ -1,6 +1,10 @@
 package nmnb.application.domain.report.service
 
 import nmnb.application.domain.report.service.dto.request.PostReportServiceRequest
+import nmnb.common.response.exception.PostException
+import nmnb.common.response.exception.ReportException
+import nmnb.common.response.status.ErrorStatus
+import nmnb.domain.post.repository.PostRepository
 import nmnb.domain.report.PostReport
 import nmnb.domain.report.repository.ReportRepository
 import nmnb.domain.user.User
@@ -11,10 +15,36 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true)
 class PostReportService(
     private val reportRepository: ReportRepository,
-) : ReportService(reportRepository) {
-    @Transactional
-    fun report(user: User, request: PostReportServiceRequest) {
-        val report = PostReport(request.targetId, user.id!!, request.contentType)
-        save(report)
+    private val postRepository: PostRepository,
+) : ReportService<PostReport, PostReportServiceRequest>(reportRepository) {
+    override fun validateReport(user: User, request: PostReportServiceRequest) {
+        validateNotSelfReport(user.id!!, request.targetId)
+        validateNotDuplicateReport(user.id!!, request.targetId)
+    }
+
+    private fun validateNotSelfReport(
+        reporterId: String,
+        targetId: Long,
+    ) {
+        val post = postRepository.findById(targetId).orElseThrow {
+            PostException(ErrorStatus.POST_NOTFOUND)
+        }
+
+        if (post.user.id == reporterId) {
+            throw ReportException(ErrorStatus.CANNOT_REPORT_SELF)
+        }
+    }
+
+    private fun validateNotDuplicateReport(
+        reporterId: String,
+        targetId: Long,
+    ) {
+        if (reportRepository.existsPostReport(reporterId, targetId)) {
+            throw ReportException(ErrorStatus.ALREADY_REPORTED)
+        }
+    }
+
+    override fun createReport(user: User, request: PostReportServiceRequest): PostReport {
+        return PostReport(request.targetId, user.id!!, request.contentType)
     }
 }
