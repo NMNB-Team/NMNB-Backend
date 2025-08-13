e369b7e73bb89fc2c7d8e8b783db6652ae59fe5f
88dldl <chltnrgml0999@gmail.com>
2025-08-13 23:14:21 +0900

    refactor: 신고 생성 공통 로직 제네릭화 및 부모 클래스 리팩토링

--- nmnb-application/src/main/kotlin/nmnb/application/domain/report/service/ReportService.kt
+++ nmnb-application/src/main/kotlin/nmnb/application/domain/report/service/ReportService.kt
@@ -2,9 +2,19 @@ package nmnb.application.domain.report.service
 
 import nmnb.domain.report.Report
 import nmnb.domain.report.repository.ReportRepository
+import nmnb.domain.user.User
+import org.springframework.transaction.annotation.Transactional
 
-abstract class ReportService(
+abstract class ReportService<R : Report, Request>(
     private val reportRepository: ReportRepository,
 ) {
-    fun save(report: Report) = reportRepository.save(report)
+    fun save(report: R) = reportRepository.save(report)
+
+    @Transactional
+    open fun report(user: User, request: Request) {
+        val report = createReport(user, request)
+        save(report)
+    }
+
+    abstract fun createReport(user: User, request: Request): R
 }
