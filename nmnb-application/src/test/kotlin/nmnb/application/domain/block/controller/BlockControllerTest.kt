6651975ec040fb3aeb06c3c81340734066d8c83e
88dldl <chltnrgml0999@gmail.com>
2025-08-14 17:10:36 +0900

    test: 사용자 차단 해제 요청 테스트 작성

--- nmnb-application/src/test/kotlin/nmnb/application/domain/block/controller/BlockControllerTest.kt
+++ nmnb-application/src/test/kotlin/nmnb/application/domain/block/controller/BlockControllerTest.kt
@@ -12,6 +12,7 @@ import org.mockito.kotlin.whenever
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
@@ -31,7 +32,34 @@ class BlockControllerTest : ControllerTestSupport() {
         doNothing().whenever(userBlockService).block(any(), any())
 
         mockMvc.perform(
-            post("/v1/api/blocks")
+            post("/v1/api/users/blocks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-Access-Token", accessToken)
+                .header("Device-Id", deviceId)
+                .with(csrf())
+                .content(
+                    objectMapper.writeValueAsString(request),
+                ),
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(SuccessStatus.OK.code))
+    }
+
+    @WithMockUser
+    @DisplayName("차단 해제 요청에 성공한다")
+    @Test
+    fun unBlock() {
+        val deviceId = "deviceId"
+        val accessToken = "access.token"
+        val user = User.fixture()
+
+        mockUserAuthentication(user, accessToken, deviceId)
+        val request = UserBlockRequest("")
+
+        doNothing().whenever(userBlockService).unBlock(any(), any())
+
+        mockMvc.perform(
+            delete("/v1/api/users/blocks")
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("X-Access-Token", accessToken)
                 .header("Device-Id", deviceId)
