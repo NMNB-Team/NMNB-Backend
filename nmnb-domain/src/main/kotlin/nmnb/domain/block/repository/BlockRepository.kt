7f50bf9409e54b58ea6dddaf4a59f4647838aad3
88dldl <chltnrgml0999@gmail.com>
2025-08-14 15:44:41 +0900

    feat: 차단한 사용자 id 조회 기능 추가

--- nmnb-domain/src/main/kotlin/nmnb/domain/block/repository/BlockRepository.kt
+++ nmnb-domain/src/main/kotlin/nmnb/domain/block/repository/BlockRepository.kt
@@ -1,6 +1,11 @@
 package nmnb.domain.block.repository
 
+import io.lettuce.core.dynamic.annotation.Param
 import nmnb.domain.block.Block
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface BlockRepository : JpaRepository<Block, Long>
+interface BlockRepository : JpaRepository<Block, Long> {
+    @Query("SELECT b.blockedUser.id FROM Block b WHERE b.blocker.id=:userId")
+    fun findByBlockerId(@Param("userId") userId: String): List<String>
+}
