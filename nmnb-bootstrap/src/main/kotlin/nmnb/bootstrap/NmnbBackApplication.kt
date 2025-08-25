97f9e7dafc7b7b95f2ab45602df484d1b2206d2f
88dldl <chltnrgml0999@gmail.com>
2025-08-26 00:51:05 +0900

    refactor: RedisConfig에 RedisRepositories 설정 이동

--- nmnb-bootstrap/src/main/kotlin/nmnb/bootstrap/NmnbBackApplication.kt
+++ nmnb-bootstrap/src/main/kotlin/nmnb/bootstrap/NmnbBackApplication.kt
@@ -2,9 +2,7 @@ package nmnb.bootstrap
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
-@EnableRedisRepositories(basePackages = ["nmnb.common.auth.repository"])
 @SpringBootApplication(scanBasePackages = ["nmnb.common", "nmnb.domain", "nmnb.application"])
 class NmnbBackApplication
 
--- nmnb-webflux-bootstrap/src/main/kotlin/nmnb/webfluxBootstrap/NmnbWebfluxApplication.kt
+++ nmnb-webflux-bootstrap/src/main/kotlin/nmnb/webfluxBootstrap/NmnbWebfluxApplication.kt
@@ -2,9 +2,7 @@ package nmnb.webfluxBootstrap
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
-@EnableRedisRepositories(basePackages = ["nmnb.common.auth.repository"])
 @SpringBootApplication(scanBasePackages = ["nmnb.r2dbc", "nmnb.webflux", "nmnb.common"])
 class NmnbWebfluxApplication
 
