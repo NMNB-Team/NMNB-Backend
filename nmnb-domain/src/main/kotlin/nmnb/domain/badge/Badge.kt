1675fdd7fbd7568013375d91d0194b15630085b2
88dldl <chltnrgml0999@gmail.com>
2025-10-10 16:30:50 +0900

    feat: Badge 엔티티 추가

---
+++ nmnb-domain/src/main/kotlin/nmnb/domain/badge/Badge.kt
@@ -0,0 +1,33 @@
+package nmnb.domain.badge
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import nmnb.domain.JpaBaseEntity
+
+@Entity
+@Table(name = "badges")
+class Badge(
+    @Column(nullable = false, unique = true)
+    val name: String,
+
+    @Column(nullable = false)
+    val description: String,
+
+    @Column(nullable = false)
+    val price: Int,
+
+    @Column(nullable = false)
+    val imageUrl: String,
+
+    @Column(nullable = false)
+    val active: Boolean = true,
+) : JpaBaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "badge_id")
+    val id: Long? = null
+}
